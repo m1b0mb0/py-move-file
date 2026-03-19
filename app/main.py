278ea0e053925f3a05e20a0f74bb18827d1e3c6b
Illia Redka <illaredka8@gmail.com>
2026-03-19 22:16:39 +0200

    removed hardcoded separator and removed repeated code

--- app/main.py
+++ app/main.py
@@ -6,24 +6,13 @@ def move_file(command: str) -> None:
     if len(split_command) == 3:
         command_name, first_file, second_file = split_command
         if command_name == "mv":
-            if "/" in second_file:
-                parts = second_file.split("/")
-                path = ""
-                for part in parts[:-1]:
-                    path = os.path.join(path, part)
-                    if not os.path.exists(path):
-                        os.mkdir(path)
-                path = os.path.join(path, parts[-1])
-                with (
-                    open(first_file, "r") as file_in,
-                    open(path, "w") as file_out
-                ):
-                    file_out.write(file_in.read())
-                os.remove(first_file)
-            else:
-                with (
-                    open(first_file, "r") as file_in,
-                    open(second_file, "w") as file_out
-                ):
-                    file_out.write(file_in.read())
-                os.remove(first_file)
+            directory = os.path.dirname(second_file)
+            filename = os.path.basename(second_file)
+            if directory:
+                os.makedirs(directory, exist_ok=True)
+            with (
+                open(first_file, "r") as file_in,
+                open(os.path.join(directory, filename), "w") as file_out
+            ):
+                file_out.write(file_in.read())
+            os.remove(first_file)
